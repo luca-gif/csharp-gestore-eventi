5988418ba6ce8f8f9b831c532a6baf20c4388623
luca-gif <luca.89m@gmail.com>
2022-09-26 11:45:12 +0200

    Milestone 2

--- Program.cs
+++ Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Chiedo all'utente di inserire il titolo
+Console.WriteLine("Inserisci il nome dell'evento");
+string nuovoTitolo = Console.ReadLine();
+
+//Chiedo all'utente di inserire il la data
+Console.WriteLine("Inserisci la data dell'evento (dd/mm/yyyy)");
+DateTime nuovaData = Convert.ToDateTime(Console.ReadLine());
+
+//Chiedo all'utente di inserire la capienza massima
+Console.WriteLine("Inserisci il numero di posti totali");
+int nuovoMaxPosti = Convert.ToInt32(Console.ReadLine());
+
+//Instanzio un nuovo evento con le info passate dall'utente
+Evento mioEvento = new Evento(nuovoTitolo, nuovaData, nuovoMaxPosti);
+
+//Chiedo all'utente se vuole prenotare dei posti
+Console.WriteLine("Vuoi prenotare dei posti?");
+string risposta = Console.ReadLine();
+
+if(risposta == "si")
+{
+    //Chiedo all'utente quante prenotazioni vuole effettuare
+    Console.WriteLine("Quanti posti desideri prenotare?");
+    int postiPrenotati = Convert.ToInt32(Console.ReadLine());
+
+    mioEvento.PrenotaPosti(postiPrenotati);
+
+    Console.WriteLine("Numero di posti disponibili" + (mioEvento.CapienzaMax - postiPrenotati));
+}
+else
+{
+    Console.WriteLine("Ok, buona giornata");
+}
