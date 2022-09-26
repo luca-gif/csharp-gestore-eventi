d30200a521fd5a031b5066a3f07066a00c610d34
luca-gif <luca.89m@gmail.com>
2022-09-26 15:28:24 +0200

    Inizio milestone 4

--- Program.cs
+++ Program.cs
@@ -49,3 +49,27 @@ if(disdire == "si")
     Console.WriteLine("Numero di posti disponibili " + (mioEvento.CapienzaMax));
     Console.WriteLine("Numero di posti prenotati " + (mioEvento.PostiPrenotati));
 }
+
+Console.WriteLine("Qual è il titolo del tuo evento?");
+string titoloEvento = Console.ReadLine();
+
+ProgrammaEventi nuovoEvento = new ProgrammaEventi(titoloEvento);
+
+Console.WriteLine("Quanti eventi vuoi aggiungere?");
+int n = Convert.ToInt32(Console.ReadLine());
+List<Evento> eventi = new List<Evento>();
+
+for(int i = 0; i < n; i++)
+{
+    Console.WriteLine($"Scrivi il titolo del {i + 1}° evento");
+    string titolo = Console.ReadLine();
+
+    Console.WriteLine($"Inserisci la data (DD/MM/YYYY) {i + 1}° evento");
+    DateTime data = Convert.ToDateTime(Console.ReadLine());
+
+    Console.WriteLine($"Inserisci il numero di posti totali del {i + 1}° evento");
+    int maxPosti = Convert.ToInt32(Console.ReadLine());
+
+     (new Evento(titolo, data, maxPosti));
+}
+
--- ProgrammaEventi.cs
+++ ProgrammaEventi.cs
@@ -69,5 +69,4 @@ public class ProgrammaEventi
         return titolo;
     }
 
-
 }
